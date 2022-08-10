94cf0b335d8c7c428ff4cc668d637652e802b42a
Abel Mendez <abel.m.villagra@gmail.com>
2022-08-10 12:31:06 -0300

    limpiando console log

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -2,7 +2,6 @@ const { default: axios } = require("axios");
 const getMovies= async ( req,res )=>{
     try {
         const apiData= await axios.get(`http://www.omdbapi.com/?t=${ req.query.title }&y=${ req.query.year }&type=${ req.query.type }&apikey=32b0c60c`);
-        console.log(`http://www.omdbapi.com/?t=${ req.query.title }&y=${ req.query.year }&type=${ req.query.type }&apikey=32b0c60c`);
         res.json( {
             "data": apiData.data,
             "status": 200,
